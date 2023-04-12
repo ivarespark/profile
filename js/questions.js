42f7589ab8bffeb8a4b6ad72e9e6675939c89bbb
Ivan Espinosa <ivarespar@gmail.com>
2023-04-11 23:55:07 -0500

    Primer commit. Copia plantilla

---
+++ js/questions.js
@@ -0,0 +1,20 @@
+(function(){
+    const titleQuestions = [...document.querySelectorAll('.question_title')];
+    titleQuestions.forEach(question => {
+        question.addEventListener('click', ()=>{
+            let height = 0;
+            let answer = question.nextElementSibling;
+            let addPadding =  question.parentElement.parentElement;
+
+            question.children[0].classList.toggle('question_arrow--rotate');
+            addPadding.classList.toggle('question_padding--add');
+
+            if(answer.clientHeight === 0){
+                height = answer.scrollHeight;
+            }
+
+            answer.style.height = `${height}px`;
+        });
+    });
+
+})();
